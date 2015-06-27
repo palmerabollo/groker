3bfd0189e135d14a3da6af7ee26638660dd80b63
Valere JEANTET <valere.jeantet@gmail.com>
2015-06-27 23:02:13 +0200

    Change to match the gemsi/grok New() usage update

--- groker.go
+++ groker.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "bufio"
-    "os"
-    "encoding/json"
-    "github.com/gemsi/grok"
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/gemsi/grok"
 )
 
 func main() {
-    pattern := flag.String("pattern", "%{GREEDYDATA:msg}", "a grok expression")
-    namedcapture := flag.Bool("namedcapture", false, "parse only named captures (default is false)")
-    flag.Parse()
+	pattern := flag.String("pattern", "%{GREEDYDATA:msg}", "a grok expression")
+	namedcapture := flag.Bool("namedcapture", false, "parse only named captures (default is false)")
+	flag.Parse()
 
-    var g *grok.Grok
+	var g *grok.Grok
 
-    if *namedcapture {
-      g = grok.New(grok.NAMEDCAPTURE)
-    } else {
-      g = grok.New();
-    }
+	if *namedcapture {
+		g = grok.NewWithConfig(&grok.Config{NamedCapturesOnly: true})
+	} else {
+		g = grok.New()
+	}
 
-    scanner := bufio.NewScanner(os.Stdin)
-    for scanner.Scan() {
-        line := scanner.Text()
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
 
-        values, _ := g.Parse(*pattern, line)
-        delete(values, "")
+		values, _ := g.Parse(*pattern, line)
+		delete(values, "")
 
-        encoded, _ := json.Marshal(values)
-        fmt.Println(string(encoded))
-    }
+		encoded, _ := json.Marshal(values)
+		fmt.Println(string(encoded))
+	}
 }
